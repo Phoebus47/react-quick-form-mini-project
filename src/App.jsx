f7703f2644f49fd1dcfd5c283754239d45c069b3
Thanakrit <thanakrit.than.biz@gmail.com>
2025-03-21 16:22:00 +0700

    feat: add form section to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import { Form } from "./components/Form";
 
 function App() {
-
   return (
     <>
-      <h1>React Quick Form Mini Project</h1>
+      <Form />
     </>
   );
 }
